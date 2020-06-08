1cd4af252ec6b1a35320e2ed3216f3ecfe6a6643
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-06-08 21:28:46 +0300

    Add Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,22 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      color: Colors.grey,
-      home: SafeArea(
-              child: Column(
-          children: <Widget>[
-            Center(child: Text("lorem ipsum")),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(onPressed: (){}, child: Text("YES")),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FlatButton(onPressed: (){}, child: Text("NO")),
-            ),
-          ],
+      color: Colors.black26[200],
+      home: Scaffold(
+              body: SafeArea(
+                child: Column(
+            children: <Widget>[
+              Center(child: Text("lorem ipsum")),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(onPressed: (){}, child: Text("YES")),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(onPressed: (){}, child: Text("NO")),
+              ),
+            ],
+          ),
         ),
       ),
     );
