86784f491b739a58592a0de8a7f60217d9f2f798
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-06-18 19:23:49 +0300

    Add App background data

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,9 @@ class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      color: Colors.black26[200],
       home: Scaffold(
-              body: SafeArea(
-                child: Column(
-            children: <Widget>[
-              Center(child: Text("lorem ipsum")),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: FlatButton(onPressed: (){}, child: Text("YES")),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: FlatButton(onPressed: (){}, child: Text("NO")),
-              ),
-            ],
-          ),
-        ),
+        backgroundColor: Colors.grey[900],
+        
       ),
     );
   }
